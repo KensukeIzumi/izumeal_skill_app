77bd366e45f068c6e38330898f0c1d0d1d47a220
Kensuke Izumi <kensuke1275@gmail.com>
2017-03-31 14:38:50 +0900

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates(:name, presence: true)
-  validates(:email, presence: true)
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
