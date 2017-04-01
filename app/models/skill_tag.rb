37576fc43528738ecf931dfee90b01fa61f728d4
Kensuke Izumi <kensuke1275@gmail.com>
2017-04-01 11:52:37 +0900

    add relationships

--- app/models/skill_tag.rb
+++ app/models/skill_tag.rb
@@ -1,2 +1,4 @@
 class SkillTag < ApplicationRecord
+  has_many :user_skill_tags, dependent: :destroy
+  has_many :users, through: :user_skill_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  validates(:name, presence: true)
+  validates(:ame, presence: true)
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :user_skill_tags, dependent: :destroy
+  has_many :user_skill_tags, foreign_key: 'added_by', dependent: :destroy
+  has_many :skill_tags, through: :user_skill_tags
 
   has_secure_password
 end
