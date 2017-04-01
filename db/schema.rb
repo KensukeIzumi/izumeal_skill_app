2c299b00de33dbdd0607f1da07580b01c6e3f8cf
Kensuke Izumi <kensuke1275@gmail.com>
2017-04-01 11:18:04 +0900

    add index to middle table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330014410) do
+ActiveRecord::Schema.define(version: 20170401021338) do
+
+  create_table "skill_tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_skill_tags", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "skill_tag_id"
+    t.integer  "added_by"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_user_skill_tags_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
