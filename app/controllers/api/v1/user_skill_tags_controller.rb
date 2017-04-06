9900b48906b5f5cc38c0cb7930a23740661edcc3
Kensuke Izumi <kensuke1275@gmail.com>
2017-04-06 11:43:26 +0900

    create user_skill_tags api

---
+++ app/controllers/api/v1/user_skill_tags_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class UserSkillTagsController < ApplicationController
+      def create 
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :skill_tags
+      resources :user_skill_tags
     end
   end
 end
